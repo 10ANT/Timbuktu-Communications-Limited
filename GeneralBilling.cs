6c084ce5bc3b784485144f7e20d0dedc1e79c3b3
Adrie John <tnt528722@gmail.com>
2023-10-06 13:19:58 -0700

    Almost Finshed, I finished debugging, and started testing instances of Class

--- GeneralBilling.cs
+++ GeneralBilling.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,24 +10,48 @@ namespace Timbuktu_Communications_Limited
 
     public class GeneralBilling
     {
-		const int GCT = 25;
+		//Testing Vars
+		public double fCTandDLcharge =7;
+
+		public const double GCT = 0.25;
 
 		public int accNum { set; get; } = 0;
 		public string accHolderName {set; get; } = string.Empty;
 		public string accHolderAddress = " ";
         public int serviceType { set; get; } = 0;
-        public double previousBalance { set; get; } = 0;
-        public double paymtonPrevBal { set; get; } = 0;
+        public static double previousBalance { set; get; } = 0;
+        public static double paymtonPrevBal { set; get; } = 0;
 
         //DateTime currentPaymentDueDate = new DateTime();
-		public DateTime currentPaymtDate { get; set; }
+		public DateTime currentPaymtDueDate { get; set; }
+		public DateTime previousPaymtDate { get; set; }
+		public DateTime previousDueDate { get; set; }
 
-		public double balBroughtForward()
+
+		//bundele bill selector
+		public virtual void bundlebillselector()
+		{
+
+		}
+
+
+
+
+		public static double balBroughtForward()
         {
             return (previousBalance - paymtonPrevBal);
         }
 
+        public double varBalBroughtForward = balBroughtForward();
+		public double vartotAmtDueBefTax = 0;
+		public double vartotAmtDue { set; get; }
 
+
+		public double varlatePaymtFees;
+
+		public double varearlyPaymtDiscount;
+
+		
 		public void getAccHolderAddress()
 		{
 			// Variable to store the address as a single string
@@ -45,8 +70,72 @@ namespace Timbuktu_Communications_Limited
 
 			// Combine the address components into a single string
 			accHolderAddress = $"{street}, {city}, {parish} {postalCode}";
+
+
+		}
+		public void latePaymtFees()
+		{
+			if (currentPaymtDueDate < previousPaymtDate)
+			{
+				varlatePaymtFees= 250.0;
+			}
+			else
+			{
+				varlatePaymtFees = 0.0;
+			}
+		}
+		public void earlyPaymtDiscount()
+		{
+
+			if (previousPaymtDate < currentPaymtDueDate)
+			{
+				varearlyPaymtDiscount = 250.0;
+			}
+			else
+			{
+				varearlyPaymtDiscount = 0.0;
+			}
 		}
 
+		public void lateorearlystatus()
+		{
+			if (varlatePaymtFees == 250.0)
+			{
+				Console.WriteLine(" \nPayment made after or on the due date. Late payment fees applied...");
+			}
+			else if (varlatePaymtFees == 0)
+			{
+				Console.WriteLine(" \nPayment made before the due date. No Late payment fees applied.");
+			}
+			if (varearlyPaymtDiscount == 250)
+			{
+				Console.WriteLine(" \n \x1b[1mApplying early payment discount... \x1b[0m");
+			}
+			else if (varearlyPaymtDiscount == 0)
+			{
+				Console.WriteLine(" \n \x1b[1mNo early payment discount applied.\x1b[0m");
+			}
+		}
+			
+		//servcharge
+		public double servcharge { set; get; }
+
+		public virtual void totAmtDuteBefTax( double servcharge)
+        {
+            vartotAmtDueBefTax= ((servcharge + varBalBroughtForward ) + varlatePaymtFees - varearlyPaymtDiscount);
+        }
+		//this one is to for overide of derived classes its no parameterized
+		public virtual void totAmtDuteBefTax()
+		{
+			vartotAmtDueBefTax = ((servcharge + varBalBroughtForward) + varlatePaymtFees - varearlyPaymtDiscount);
+		}
+
+		public double totAmountDue()
+		{
+			return (vartotAmtDueBefTax - (vartotAmtDueBefTax * GCT));
+		}
+		//CHECK IF LOGIC WORKS OUT
+		
 
 		public double CableTeleOnly = 2050.76;
         public double DigitalLandOnly = 1210.50;
@@ -78,20 +167,134 @@ namespace Timbuktu_Communications_Limited
             this.InternetServOnly = intServonly;
 
         }
-        public GeneralBilling() {}
+        public GeneralBilling() {
+
+		}
 
 
-    }
+	}
     public class CableTeleBill : GeneralBilling
     {
+		public double fcabtelecharge { set; get; }
 
-    }
-    public class DigitalLandBill : GeneralBilling
+		public override void totAmtDuteBefTax()
+		{
+			vartotAmtDueBefTax = ((CableTeleOnly + varBalBroughtForward) + varlatePaymtFees - varearlyPaymtDiscount);
+			fcabtelecharge = totAmountDue();
+			vartotAmtDue = totAmountDue();
+		}
+		
+
+	}
+	public class DigitalLandBill : GeneralBilling
     {
+		public double fdigitallandcharge { set; get; }
 
-    }
+		public override void totAmtDuteBefTax()
+		{
+			vartotAmtDueBefTax = ((DigitalLandOnly + varBalBroughtForward) + varlatePaymtFees - varearlyPaymtDiscount);
+			fdigitallandcharge = totAmountDue();
+			vartotAmtDue = totAmountDue();
+		}
+	}
 	public class InternetServBill : GeneralBilling
 	{
+		public double finternetservcharge { set; get; }
+
+		public override void totAmtDuteBefTax()
+		{
+			vartotAmtDueBefTax = ((InternetServOnly + varBalBroughtForward) + varlatePaymtFees - varearlyPaymtDiscount);
+			finternetservcharge = totAmountDue();
+			vartotAmtDue = totAmountDue();
+		}
+
+	}
+	public class BundledBill:GeneralBilling
+	{
+		public double bundlebillservcharge { set; get; }
+		public override void bundlebillselector()
+		{
+			while (true)
+			{
+				Console.Clear();
+				Console.WriteLine("Menu:");
+				Console.WriteLine("1. Cable Television + Digital Landline");
+				Console.WriteLine("2. Cable Television + Internet service ");
+				Console.WriteLine("3. Digital Landline + Internet service");
+				Console.WriteLine("4. Cable Television + Digital Landline + Internet service");
+				Console.WriteLine("5. Exit");
+
+				Console.Write("Enter your choice: ");
+				string choice = Console.ReadLine();
+
+				switch (choice)
+				{
+					case "1":
+						Console.WriteLine("You selected Option 1. Cable Television + Digital Landline");
+						// Perform action for Option 1
+						//settings a varib;le to dual serv charge
+						bundlebillservcharge = CableandDigitalCharge();
+						//calculating tot before tax
+						totAmtDuteBefTax(this.bundlebillservcharge);
+						//setting total moutn for corresponding serv charge
+
+						 fCTandDLcharge = totAmountDue();
+
+						break;
+					case "2":
+						Console.WriteLine("You selected Option 2.Cable Television + Internet service");
+						// Perform action for Option 2
+						//settings a varib;le to dual serv charge
+						bundlebillservcharge = CableandInterServCharge();
+						//calculating tot before tax
+						totAmtDuteBefTax(this.bundlebillservcharge);
+						//setting total moutn for corresponding serv charge
+						double fCTandIScharge = totAmountDue();
+
+						break;
+					case "3":
+						Console.WriteLine("You selected Option 3.Digital Landline + Internet service");
+						// Perform action for Option 3
+						//settings a varib;le to dual serv charge
+						bundlebillservcharge = DigiLineandInternetServCharge();
+						//calculating tot before tax
+						totAmtDuteBefTax(this.bundlebillservcharge);
+						//setting total moutn for corresponding serv charge
+						double fDLandIScharge = totAmountDue();
+
+						break;
+					case "4":
+						Console.WriteLine("You selected Option 4. Cable Television + Digital Landline + Internet service");
+						// Perform action for Option 4
+						//settings a varib;le to dual serv charge
+						bundlebillservcharge = Cable_DigiLine_andInternetServCharge();
+						//calculating tot before tax
+						totAmtDuteBefTax(this.bundlebillservcharge);
+						//setting total moutn for corresponding serv charge
+						double fCT_DLandIScharge = totAmountDue();
+
+						break;
+					case "5":
+						Console.WriteLine("Exiting program.");
+						return;
+
+					default:
+						Console.WriteLine("Invalid choice. Please try again.");
+						break;
+				}
+
+				Console.Write("Press any key to continue...");
+				Console.ReadKey();
+				break;
+			}
+
+		}
+		public override void totAmtDuteBefTax()
+		{
+			vartotAmtDueBefTax = ((bundlebillservcharge + varBalBroughtForward) + varlatePaymtFees - varearlyPaymtDiscount);
+			vartotAmtDue = totAmountDue();
+		}
+
 
 	}
 
--- Program.cs
+++ Program.cs
@@ -1,9 +1,78 @@
-﻿using Timbuktu_Communications_Limited;
+﻿using System.Globalization;
+using System.Reflection.Emit;
+using System.Security.Cryptography.X509Certificates;
+using Timbuktu_Communications_Limited;
 
 public class TimbuktuCom
 {
-    private static void Main(string[] args)
-    {/*
+	public static GeneralBilling customer1 = null; // Use the common base class
+
+	private static void Main(string[] args)
+    {
+		Console.WriteLine("Select the type of customer you are:");
+		Console.WriteLine("1. Cable Television Only");
+		Console.WriteLine("2. Digital Landline Only");
+		Console.WriteLine("3. Internet service Only");
+		Console.WriteLine("4. Bundle Bill Service");
+
+		
+
+		int choice = int.Parse(Console.ReadLine());
+
+		switch (choice)
+		{
+			case 1:
+				
+				customer1 = new CableTeleBill();
+				dateCollectandParsePreviouspaymt();
+				dateCollectandParseCurDuepaymt();
+
+				customer1.earlyPaymtDiscount();
+				customer1.latePaymtFees();
+
+
+				customer1.lateorearlystatus();
+				customer1.totAmtDuteBefTax();
+				Console.WriteLine(customer1.vartotAmtDueBefTax);
+				Console.WriteLine(customer1.vartotAmtDue);
+
+				break;
+			case 2:
+				customer1 = new DigitalLandBill();
+				customer1.totAmtDuteBefTax();
+				Console.WriteLine(customer1.vartotAmtDueBefTax);
+				Console.WriteLine(customer1.vartotAmtDue);
+
+				break;
+			case 3:
+			
+				customer1 = new InternetServBill();
+				break;
+
+			case 4:
+				customer1 = new BundledBill();
+				customer1.bundlebillselector();
+
+				customer1.totAmtDuteBefTax();
+				Console.WriteLine(customer1.vartotAmtDueBefTax);
+				Console.WriteLine(customer1.vartotAmtDue);
+                Console.WriteLine(customer1.fCTandDLcharge);
+
+
+
+                break;
+
+			default:
+				Console.WriteLine("Invalid choice");
+				return;
+		}
+
+
+
+
+
+
+		/*
         Console.WriteLine("Hello, World!");
         GeneralBilling gen = new GeneralBilling();
         Console.WriteLine("$"+gen.CableandDigitalCharge());
@@ -11,17 +80,128 @@ public class TimbuktuCom
         InternetBilling mina = new InternetBilling();
         Console.WriteLine(mina.CableTeleOnly);
         */
-        Customer customer = new Customer();
-        //customer.getAccHolderAddress();
+		//Customer customew = new Customer();
+		//customer.getAccHolderAddress();
 
 
-        Console.WriteLine(customer.CableTeleOnly); 
-        CableTeleBill newcu = new CableTeleBill();
+		//Console.WriteLine(customer.CableTeleOnly);
 
-        newcu.currentPaymtDate = new DateTime(2023, 5, 6);
-        Console.WriteLine(newcu.currentPaymtDate);
+
+		//newcu.currentPaymtDate = new DateTime(2023, 5, 6);
+		
+		//customer1.currentPaymtDueDate= new DateTime(2023, 4, 6);
+		//customer1.previousPaymtDate = new DateTime(2023, 5, 6);
+
+
+
+
+		//Console.WriteLine(customer1.latePaymtFees());
+		//Console.WriteLine(customer1.earlyPaymtDiscount());
+		
+
+
+
+
+
 
     }
+	
+	public static void dateCollectandParseCurDuepaymt()
+    {
+		
+		//collect date
+		Console.Write("Enter your current payment date (yyyy-MM-dd format): ");
+		string userInput = Console.ReadLine();
+
+		DateTime parseHolder;
+		
+
+		// Attempt to parse the user input into a DateTime
+		if (DateTime.TryParse(userInput, out parseHolder))
+		{
+			// Parsing successful, assign the value to newcu.currentPaymtDate
+			 //Console.WriteLine($"You entered: {parseHolder.ToString("yyyy-MM-dd")}");
+			 customer1.currentPaymtDueDate = parseHolder;
+		
+		}
+		else
+		{
+			// Parsing failed, handle the error
+			Console.WriteLine("Invalid date input. Please enter a valid date in yyyy-MM-dd format.");
+           
+        }
+	}
+
+	public static void dateCollectandParsePreviouspaymt()
+	{
+
+		//collect date
+		Console.Write("Enter your previous payment date (yyyy-MM-dd format): ");
+		string userInput = Console.ReadLine();
+
+		DateTime parseHolder;
+
+
+		// Attempt to parse the user input into a DateTime
+		if (DateTime.TryParse(userInput, out parseHolder))
+		{
+			// Parsing successful, assign the value to newcu.currentPaymtDate
+			 //Console.WriteLine($"You entered: {parseHolder.ToString("yyyy-MM-dd")}");
+			 customer1.previousPaymtDate = parseHolder;
+		}
+		else
+		{
+			// Parsing failed, handle the error
+			Console.WriteLine("Invalid date input. Please enter a valid date in yyyy-MM-dd format.");
+		}
+	}
+
+	public static void dateCollectandPreviousDueDate()
+	{
+
+		//collect date
+		Console.Write("Enter your previous due date (yyyy-MM-dd format): ");
+		string userInput = Console.ReadLine();
+
+		DateTime parseHolder;
+
+
+		// Attempt to parse the user input into a DateTime
+		if (DateTime.TryParse(userInput, out parseHolder))
+		{
+			// Parsing successful, assign the value to newcu.currentPaymtDate
+			//Console.WriteLine($"You entered: {parseHolder.ToString("yyyy-MM-dd")}");
+			customer1.previousDueDate = parseHolder;
+		}
+		else
+		{
+			// Parsing failed, handle the error
+			Console.WriteLine("Invalid date input. Please enter a valid date in yyyy-MM-dd format.");
+		}
+	}
+
+	
+
+	/*public static void CollectAndParsePrev(Customer customer)
+	{
+		Console.Write("Enter your current payment date (yyyy-MM-dd format): ");
+		string userInput = Console.ReadLine();
+
+		DateTime currentPaymentDate;
+
+		if (!string.IsNullOrWhiteSpace(userInput) &&
+			DateTime.TryParseExact(userInput, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out currentPaymentDate))
+		{
+			customer.currentPaymtDueDate = currentPaymentDate;
+
+			
+		}
+		else
+		{
+			Console.WriteLine("Invalid date input. Please enter a valid date in yyyy-MM-dd format.");
+		}
+	}*/	
 }
+// to string  to get rid og 12:00:00am
 
 
